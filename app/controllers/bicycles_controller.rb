905758d59b287f1d5851ac19fe2f7360beea75af
Atsushi-Endo <ena-choco@live.jp>
2020-07-22 23:37:45 +0900

    不要な部分の削除

--- app/controllers/bicycles_controller.rb
+++ app/controllers/bicycles_controller.rb
@@ -2,22 +2,7 @@ class BicyclesController < ApplicationController
   def index
   end
 
-  def search
-    latitude = params[:latitude]
-    longitude = params[:longitude]
-  
-    @places = Place.all.within(2, origin: [latitude, longitude])
-  end
-
   def search_location
-    latitude = params[:latitude].to_f
-    longitude = params[:longitude].to_f
-
-    gon.lat = params[:latitude].to_f
-    gon.lng = params[:longitude].to_f
-    gon.location = Place.within_box(0.310686, latitude, longitude)
-
-    @locations = Place.within_box(0.310686, latitude, longitude)
-
   end
+
 end
