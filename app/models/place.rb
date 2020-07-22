9e516273f244d2b3e27ce4b69a549fabae36c97a
Atsushi-Endo <ena-choco@live.jp>
2020-07-22 23:37:05 +0900

    不要になったので削除

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,16 +1,2 @@
 class Place < ApplicationRecord
-  geocoded_by :address
-  after_validation :geocode, if: :address_changed?
-
-  # window.location.href = "/bicycles/search_location?latitude="+position.coords.latitude+"&longitude="+position.coords.longitude
-
-  class << self
-    def within_box(distance, latitude, longitude)
-        distance = distance
-        center_point = [latitude, longitude]
-        box = Geocoder::Calculations.bounding_box(center_point, distance)
-        self.within_bounding_box(box)
-    end
-  end 
-
 end
